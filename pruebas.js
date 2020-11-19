f2443c7eca4dc751fe6fb51c9a07570f195da19d
carloseag1609 <carloseag1233@gmail.com>
2020-11-18 21:36:55 -0600

    ajsdkhasjdk

--- pruebas.js
+++ pruebas.js
@@ -1,12 +1,24 @@
-const S = [3, 1, 4, 2, 3];
-let hayRepetidos = false;
+// const S = [3, 1, 4, 2, 3];
+// let hayRepetidos = false;
 
-for(let i = 0; i < S.length; i++) {
-    for(let j = i + 1; j < S.length; j++) {
-        if(S[i] === S[j]) {
-            hayRepetidos = true;
-        }
+// for(let i = 0; i < S.length; i++) {
+//     for(let j = i + 1; j < S.length; j++) {
+//         if(S[i] === S[j]) {
+//             hayRepetidos = true;
+//         }
+//     }
+// }
+
+// hayRepetidos? console.log('Hay repetidos') : console.log('No hay repetidos');
+
+
+const n = 5;
+
+function factorial(n = 5) {
+    if (n == 1) {
+        return 1;
     }
+    return (n * factorial(n - 1));
 }
 
-hayRepetidos? console.log('Hay repetidos') : console.log('No hay repetidos');
+console.log(factorial(n));
